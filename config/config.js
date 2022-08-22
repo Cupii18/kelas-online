e9d69b66a6da05177fd2757b98aeb3c46da51a9e
Cupii18 <ekamayanauranabila@gmail.com>
2022-08-22 20:40:41 +0700

    lagi

--- config/config.js
+++ config/config.js
@@ -1,16 +1,45 @@
 
 module.exports = {
-  client: 'mysql',
-  connection: {
-    host : '127.0.0.1',
-    port : 3306,
-    user : 'root',
-    password : '',
-    database : 'kelaso-online'
-  }
-};
+    "development": {
+      "username": "root",
+      "password": null,
+      "database": "kelas_online",
+      "host": "127.0.0.1",
+      "dialect": "mysql",
+      "define": {
+        "charset": "utf8mb4",
+        "collate": "utf8mb4_unicode_ci",
+        "timestamps": false
 
+        },
+        "logging": false
+    },
+    "test": {
+      "username": "root",
+      "password": null,
+      "database": "kelas_online",
+      "host": "127.0.0.1",
+      "dialect": "mysql",
+      "define": {
+        "charset": "utf8mb4",
+        "collate": "utf8mb4_unicode_ci",
+        "timestamps": false
+
+        },
+        "logging": false
+    },
+    "production": {
+        "username": "root",
+        "password": null,
+        "database": "kelas_online",
+        "host": "127.0.0.1",
+        "dialect": "mysql",
+        "define": {
+          "charset": "utf8mb4",
+          "collate": "utf8mb4_unicode_ci",
+          "timestamps": false
   
-  
-    
-  
+          },
+        "logging": false
+    }
+  }
